8321309584810fafcd5066bd0c6efac9285b8f44
Stephen <stephen@remine.com>
2019-11-09 13:38:11 -0600

    building controllers

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,8 +1,60 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const getMany = model => async (req, res) => {}
+  try {
+    const doc = await model
+      .findOne({ createdBy: userId, _id: id })
+      .lean()
+      .exec()
 
-export const createOne = model => async (req, res) => {}
+    if (!doc) {
+      return res.status(404).end()
+    }
+
+    res.status(200).json({ data: doc })
+  } catch (error) {
+    console.error(error)
+    res.status(404).end()
+  }
+}
+
+export const getMany = model => async (req, res) => {
+  try {
+    const doc = await model
+      .find({ createdBy: req.user._id })
+      .lean()
+      .exec()
+
+    if (!doc) {
+      res.sendStatus(400)
+    }
+
+    res.status(200).json({ data: doc })
+  } catch (error) {
+    console.error(error)
+    res.status(400).end()
+  }
+}
+
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({ ...req.body, createdBy: req.user._id })
+  return res.status(201).send({ data: doc })
+
+  //   try {
+  //     const doc = await model
+  //       .create({ ...req.body, createdBy: req.user._id })
+  //       .lean()
+  //       .exec()
+  //     if (!doc) {
+  //       res.status(400).end()
+  //     }
+  //     res.status(201).send({ data: doc })
+  //   } catch (error) {
+  //     console.error(error)
+  //     res.status(400).end()
+  //   }
+}
 
 export const updateOne = model => async (req, res) => {}
 
