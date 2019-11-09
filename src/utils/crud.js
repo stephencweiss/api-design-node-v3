6da5844c0f897a017d70b50ceed367ec1afc39b8
Stephen <stephen@remine.com>
2019-11-09 14:36:53 -0600

    finish out generic controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -38,27 +38,53 @@ export const getMany = model => async (req, res) => {
 }
 
 export const createOne = model => async (req, res) => {
-  const doc = await model.create({ ...req.body, createdBy: req.user._id })
-  return res.status(201).send({ data: doc })
+  const createdBy = req.user._id
 
-  //   try {
-  //     const doc = await model
-  //       .create({ ...req.body, createdBy: req.user._id })
-  //       .lean()
-  //       .exec()
-  //     if (!doc) {
-  //       res.status(400).end()
-  //     }
-  //     res.status(201).send({ data: doc })
-  //   } catch (error) {
-  //     console.error(error)
-  //     res.status(400).end()
-  //   }
+  try {
+    const doc = await model.create({ ...req.body, createdBy })
+
+    if (!doc) {
+      res.status(400).end()
+    }
+    res.status(201).json({ data: doc })
+  } catch (error) {
+    console.error(error)
+    res.status(400).end()
+  }
 }
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  try {
+    const doc = await model.findOneAndUpdate(
+      { createdBy: req.user._id, _id: req.params.id },
+      { ...req.body },
+      { new: true }
+    )
+    if (!doc) {
+      return res.status(400).end()
+    }
+    res.status(200).json({ data: doc })
+  } catch (error) {
+    console.error(error)
+    res.status(400).end()
+  }
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  try {
+    const doc = await model.findOneAndRemove(
+      { createdBy: req.user._id, _id: req.params.id },
+      { ...req.body }
+    )
+    if (!doc) {
+      return res.status(400).end()
+    }
+    res.status(200).json({ data: doc })
+  } catch (error) {
+    console.error(error)
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
